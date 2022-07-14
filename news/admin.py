5bc5ac009946cf422cd4f9126169014955aa1dba
Wesson <vinokurov.filipp1@yandex.ru>
2022-07-14 22:55:17 +0300

    made news admin, learned admin

--- news/admin.py
+++ news/admin.py
@@ -1,17 +1,46 @@
 from django.contrib import admin
+from django.db.models import QuerySet
+
 from news.models import News, Comment, ActivityFlag
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'username']
 
 
 @admin.register(ActivityFlag)
 class ActivityFlagAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name']
 
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    pass
+
+    actions = ['mark_as_murder', 'mark_as_games', 'mark_as_technologies']
+
+    def mark_as_murder(self, request, queryset: QuerySet):
+        queryset.update(status='m')
+
+    def mark_as_games(self, request, queryset: QuerySet):
+        queryset.update(status='g')
+
+    def mark_as_technologies(self, request, queryset: QuerySet):
+        queryset.update(status='t')
+
+    mark_as_games.short_desription = 'Присвоить статус "Игры"'
+    mark_as_murder.short_desription = 'Присвоить статус "Убийство"'
+    mark_as_technologies.short_desription = 'Присвоить статус "Технологии"'
+    list_display = ['id', 'title', 'activity_flag']
+    list_filter = ['activity_flag', 'title']
+    search_fields = ['title', 'content']
+    fieldsets = (
+        ('Основные сведения', {
+            'fields': ('title', 'content'),
+            'classes': ['collapse']
+        }),
+        ('Доп.поля', {
+            'fields': ('activity_flag', 'activity_flag2'),
+            'classes': ['collapse']
+        })
+    )
--- news/models.py
+++ news/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 
 class News(models.Model):
+    STATUS_CHOICES = [
+        ('m', 'Murder'),
+        ('g', 'Games'),
+        ('t', 'Technologies'),
+    ]
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=2000)
     date_added = models.DateTimeField(auto_now_add=True)
     date_edited = models.DateTimeField(auto_now=True)
     activity_flag = models.ForeignKey('ActivityFlag', on_delete=models.CASCADE, related_name='news')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='m')
 
     def __str__(self):
         return self.title
